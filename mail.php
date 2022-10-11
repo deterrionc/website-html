abac4b4c58f70b5c01818ff2f9f7af732936f09b
Pro Dev <progdev77@gmail.com>
2022-10-11 11:16:05 -0400

    First Commit

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "healer1064729@gmail.com";
+$sitename = "Felix Moore";
+
+$name = trim($_POST["name"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Name: $name \nEmail: $email \nText: $text";
+
+$pagetitle = "New message from the \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
